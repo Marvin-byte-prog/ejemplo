b7e5025b7137ef534dc0e5a96ee4d2f26800121a
unknown <you@example.com>
2021-02-15 14:49:48 -0600

    esto es una prueba

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 0;
+            int f=0;
+            int[,] matriz = new int[8, 5] {
+                { 1,1,1,1,1},
+                { 1,1,1,1,1},
+                { 1,1,1,1,1},
+                { 1,1,1,1,1},
+                { 1,1,1,1,1},
+                { 1,1,1,1,1},
+                { 1,1,1,1,1},
+                { 1,1,1,1,0}
+            };
+
+            for (int i = 0; i < 8; i++)
+            {
+
+                for (int j = 0; j < 5; j++)
+                {
+                    if (matriz[i,j] == 0)
+                    {
+                        f = i;
+                        c = j;
+                    }
+                    
+                }
+                
+            }
+        
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i == f || j == c)
+                    {
+                        matriz[i, j] = 0;
+                    }
+                    Console.Write(" " + matriz[i, j]);
+
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
